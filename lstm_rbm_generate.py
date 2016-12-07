00d608ba38ce474518c40b5400f0b3036c0ad967
Sam Dooman <samuel.dooman@gmail.com>
2016-12-07 17:49:29 -0500

    made primer an argument

--- lstm_rbm_generate.py
+++ lstm_rbm_generate.py
@@ -21,7 +21,7 @@ import midi_manipulation
 num = 3 #The number of songs to generate
 primer_song = 'Pop_Music_Midi/elise.midi' #The path to the song to use to prime the network
 
-def main(saved_weights_path):
+def main(saved_weights_path, primer):
     # First, build model then get pointers to params
     neural_net = lstm_rbm.LSTMNet()
     print "constructed neural net"
@@ -35,7 +35,7 @@ def main(saved_weights_path):
 
     print "created saver"
 
-    song_primer = midi_manipulation.get_song(primer_song)
+    song_primer = midi_manipulation.get_song(primer)
 
     print "got primer"
 
@@ -49,9 +49,9 @@ def main(saved_weights_path):
             print "about to run session"
             generated_music = sess.run(neural_net.generate(300), feed_dict={x: song_primer}) #Prime the network with song primer and generate an original song
             print "session finished"
-            new_song_path = "music_outputs/{}_{}".format(i, primer_song.split("/")[-1]) #The new song will be saved here
+            new_song_path = "music_outputs/{}_{}".format(i, primer.split("/")[-1]) #The new song will be saved here
             midi_manipulation.write_song(new_song_path, generated_music)
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
     
